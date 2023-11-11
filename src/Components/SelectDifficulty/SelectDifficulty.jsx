282b1206a92b44a01fe4616c139f0e19b247d8cc
Ryan Laird <rlaird6349@gmail.com>
2023-11-11 12:57:34 -0500

    Styled the select difficulty page

--- src/Components/SelectDifficulty/SelectDifficulty.jsx
+++ src/Components/SelectDifficulty/SelectDifficulty.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 import Button from '../Button/Button'
 import './SelectDifficulty.css';
 
-export default function SelectDifficulty( { setSelectedDifficulty }) {
+export default function SelectDifficulty({ setSelectedDifficulty }) {
   return (
     <div className="difficulty-container">
-        <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Easy"}/>
-        <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Medium"}/>
-        <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Hard"}/>
+      <div className="Welcome">
+        <div className="selectTitleH">
+          <h1>
+            Welcome to <i>Harmonify</i>
+          </h1>
+        </div>
+        <div className="paragraphAdjustment">
+          <b>
+            <p>
+              Harmonify! Gain Points By Guessing The Correct Song From The Given Clip! Three Strikes And You're Out!
+            </p>
+          </b>
+        </div>
+      </div>
+      <div className='selectGreyBox'>
+        <div className='selectDificulty'><i><b>Select a Difficulty:</b></i></div>
+        <div className='buttonFormating'>
+          <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Easy"} />
+          <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Medium"} />
+          <Button clickHandler={setSelectedDifficulty} route={"/game"} buttonText={"Hard"} />
+        </div>
+      </div>
+
     </div>
   )
 }
